9d7ebd0cabe2d914a87a7a5f07a794dc767d8acd
wgzeyu <785777793@qq.com>
2017-08-09 17:20:05 +0800

    只输出版本号更高的更新记录

--- updatecheck.php
+++ updatecheck.php
@@ -1,6 +1,23 @@
 <?php
+//第一次写PHP
 $filename = "updatecheck.txt";
-$file = fopen($filename, "r") or die("打开文件失败!");
-echo fread($file,filesize($filename));
-fclose($file);
+$nowver = $_POST["ver"];
+$file = base64_decode(getSubstr(file_get_contents($filename),"<base64>","</base64>"));
+$newver = getSubstr($file,"<ver>","</ver>");
+$txtecho = $txtecho.=getSubstr($file,"<head>","</head>");
+header("content-type: text/XML");
+for ($yicishu=$nowver+1; $yicishu<=$newver; $yicishu++) {
+	if (strstr($file,"<$yicishu>") != false) {
+		$txtecho = $txtecho.="\r\n<$yicishu>".getSubstr($file,"<$yicishu>","</$yicishu>")."</$yicishu>";
+	}
+} 
+echo("<base64>".base64_encode($txtecho)."</base64>");
+
+function getSubstr($str, $leftStr, $rightStr)
+{
+	$left = strpos($str, $leftStr);
+	$right = strpos($str, $rightStr,$left);
+	if($left < 0 or $right < $left) return '';
+	return substr($str, $left + strlen($leftStr), $right-$left-strlen($leftStr));
+}
 ?>
